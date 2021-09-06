ff700d93a04aaa3bec573831655a753c1733afdc
sharquan4 <tiffanylashspa@gmail.com>
2021-09-06 16:39:54 -0700

    spacing

--- portfolio.js
+++ portfolio.js
@@ -148,6 +148,7 @@ async function handleSubmit(event) {
 
 form.addEventListener("submit", handleSubmit());
 
+
 /*
 // DOMContentLoaded: LOCAL STORAGE
 document.addEventListener("DOMContentLoaded", () => {
