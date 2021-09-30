19dcbc2aac6abf1b270a48f0f377d3cc4832a990
sharquan4 <sharquan3@gmail.com>
2021-09-30 00:23:05 -0700

    use quicksort for gallery

--- portfolio.js
+++ portfolio.js
@@ -17,15 +17,32 @@ mobileMenuIcon.addEventListener('click', () => {
 // GALLERY SECTION
 // my images
 const galleryDict = {
-    "Bellevue (1): Kyle Huang": "7HuangKyleCHOICEdl0012.JPG",
-    "Bellevue (2): Kyle Huang": "7HuangKyleCHOICEdl0013.JPG",
-    "Bellevue (3): Kyle Huang": "7HuangKyleCHOICEdl0014.JPG",
-    "Bellevue (4): Kyle Huang": "7HuangKyleShowcaseOPT0002.jpg",
-    "Bellevue High School (5): Kyle Huang": "7HuangKyleCHOICEdl0010.JPG",
-    "Bellevue High School (6): Kyle Huang": "7HuangKyleCloseUp0001.JPG",
-    "Bellevue High School (7): Kyle Huang": "7HuangKyleCloseUp0002.JPG",
-    "Bellevue High School (8): Kyle Huang": "7HuangKyleCloseUp0004.JPG",
-    "Bellevue High School (9): Kyle Huang": "7HuangKyleComp2OPT1.jpg",
+    "Bellevue (01): Kyle Huang": "7HuangKyleCHOICEdl0012.JPG",
+    "Bellevue (04): Kyle Huang": "7HuangKyleShowcaseOPT0002.jpg",
+    "Bellevue High School (05): Kyle Huang": "7HuangKyleCHOICEdl0010.JPG",
+    "Bellevue High School (06): Kyle Huang": "7HuangKyleCloseUp0001.JPG",
+    "Bellevue High School (07): Kyle Huang": "7HuangKyleCloseUp0002.JPG",
+    "Bellevue High School (08): Kyle Huang": "7HuangKyleCloseUp0004.JPG",
+    "Bellevue High School (09): Kyle Huang": "7HuangKyleComp2OPT1.jpg",
+    "My Stuff (19): Kyle Huang": "7HuangKyleCHOICEopt0002.jpg",
+    "Snoqualmie (37): Kyle Huang": "7HuangKyleShowcase0010.jpg",
+    "Snoqualmie (38): Kyle Huang": "7HuangKyleShowcase0012.jpg",
+    "Snoqualmie (39): Kyle Huang": "7HuangKyleShowcase0018.jpg",
+    "Snoqualmie (40): Kyle Huang": "7HuangKyleShowcaseOPT0003.jpg",
+    "Snoqualmie (41): Kyle Huang": "7HuangKyleShowcaseOPT0004.jpg",
+    "Snoqualmie (42): Kyle Huang": "7HuangKyleShowcaseOPT0005.jpg",
+    "Snoqualmie (43): Kyle Huang": "7HuangKyleShowcaseOPT0006.jpg",
+    "Snoqualmie (20): Kyle Huang": "7HuangKyleAnselizingAsShotDL0011.JPG",
+    "Snoqualmie (21): Kyle Huang": "7HuangKyleAnselizingAsShotDL0012.JPG",
+    "Snoqualmie (22): Kyle Huang": "7HuangKyleAnselizingAsShotDL0014.JPG",
+    "Snoqualmie (23): Kyle Huang": "7HuangKyleDepthOfShotDL0018.jpg",
+    "Snoqualmie (30): Kyle Huang": "7HuangKyleLookWithinDL0023.JPG",
+    "Snoqualmie (31): Kyle Huang": "7HuangKylePSARTSHOW0016.jpg",
+    "Snoqualmie (32): Kyle Huang": "7HuangKylePSARTSHOW0018.jpg",
+    "Snoqualmie (33): Kyle Huang": "7HuangKylePSARTSHOW0032.jpg",
+    "Snoqualmie (34): Kyle Huang": "7HuangKylePSARTSHOW0037.JPG",
+    "Snoqualmie (35): Kyle Huang": "7HuangKylePerspectiveDL0004.JPG",
+    "Snoqualmie (36): Kyle Huang": "7HuangKyleShowcase0004.JPG",
     "Bellevue High School (10): Kyle Huang": "7HuangKyleLeadingLinesOPT0001.jpg",
     "Bellevue High School (11): Kyle Huang": "7HuangKylePatriotismOPT0003.jpg",
     "Bellevue High School (12): Kyle Huang": "7HuangKylePatriotismOPT0006.jpg",
@@ -35,54 +52,58 @@ const galleryDict = {
     "Bellevue High School (16): Kyle Huang": "7HuangKyleShowcase0017.JPG",
     "Bellevue High School (17): Kyle Huang": "7HuangKyleShowcase0019.JPG",
     "Bellevue High School (17): Kyle Huang": "7HuangKyleShowcaseOPT0001.jpg",
-    "Bellevue High School (18): Kyle Huang": "7HuangKyleShowcaseOPT0007.jpg",
-    "My Stuff (19): Kyle Huang": "7HuangKyleCHOICEopt0002.jpg",
-    "Snoqualmie (20): Kyle Huang": "7HuangKyleAnselizingAsShotDL0011.JPG",
-    "Snoqualmie (21): Kyle Huang": "7HuangKyleAnselizingAsShotDL0012.JPG",
-    "Snoqualmie (22): Kyle Huang": "7HuangKyleAnselizingAsShotDL0014.JPG",
-    "Snoqualmie (23): Kyle Huang": "7HuangKyleDepthOfShotDL0018.jpg",
-    "Snoqualmie (24): Kyle Huang": "7HuangKyleDepthOfShotDL0020.jpg",
+    "Bellevue High School (18): Kyle Huang": "7HuangKyleShowcaseOPT0007.jpg",    "Snoqualmie (24): Kyle Huang": "7HuangKyleDepthOfShotDL0020.jpg",
     "Snoqualmie (25): Kyle Huang": "7HuangKyleDepthOfShotDL0021.jpg",
     "Snoqualmie (26): Kyle Huang": "7HuangKyleDepthOfShotDL0024.jpg",
     "Snoqualmie (27): Kyle Huang": "7HuangKyleDepthOfShotDL0031.jpg",
     "Snoqualmie (28): Kyle Huang": "7HuangKyleDepthOfShotDL0035.jpg",
     "Snoqualmie (29): Kyle Huang": "7HuangKyleDepthOfShotDL0036.jpg",
-    "Snoqualmie (30): Kyle Huang": "7HuangKyleLookWithinDL0023.JPG",
-    "Snoqualmie (31): Kyle Huang": "7HuangKylePSARTSHOW0016.jpg",
-    "Snoqualmie (32): Kyle Huang": "7HuangKylePSARTSHOW0018.jpg",
-    "Snoqualmie (33): Kyle Huang": "7HuangKylePSARTSHOW0032.jpg",
-    "Snoqualmie (34): Kyle Huang": "7HuangKylePSARTSHOW0037.JPG",
-    "Snoqualmie (35): Kyle Huang": "7HuangKylePerspectiveDL0004.JPG",
-    "Snoqualmie (36): Kyle Huang": "7HuangKyleShowcase0004.JPG",
-    "Snoqualmie (37): Kyle Huang": "7HuangKyleShowcase0010.jpg",
-    "Snoqualmie (38): Kyle Huang": "7HuangKyleShowcase0012.jpg",
-    "Snoqualmie (39): Kyle Huang": "7HuangKyleShowcase0018.jpg",
-    "Snoqualmie (40): Kyle Huang": "7HuangKyleShowcaseOPT0003.jpg",
-    "Snoqualmie (41): Kyle Huang": "7HuangKyleShowcaseOPT0004.jpg",
-    "Snoqualmie (42): Kyle Huang": "7HuangKyleShowcaseOPT0005.jpg",
-    "Snoqualmie (43): Kyle Huang": "7HuangKyleShowcaseOPT0006.jpg",
+    "Bellevue (02): Kyle Huang": "7HuangKyleCHOICEdl0013.JPG",
+    "Bellevue (03): Kyle Huang": "7HuangKyleCHOICEdl0014.JPG",
 };
 
-const galleryImage = document.querySelector('.gallery-img');
+// const galleryImage = document.querySelector('.gallery-img');
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+// const galleryDesc = document.querySelector('#img-desc');
+// const galleryLength = Object.keys(galleryDict).length;
+// const galleryImgDesc = Object.keys(galleryDict);
+// const gallerySrc = Object.values(galleryDict);
+// let slideIdx = 0;
+
+// prevBtn.addEventListener('click', () => {
+//     galleryImage.src = `images/${gallerySrc[(slideIdx > 0) ? --slideIdx
+//         : slideIdx = galleryLength - 1]}`;
+//     const desc = galleryImgDesc[slideIdx];
+//     galleryDesc.innerHTML = desc;
+// });
+
+// nextBtn.addEventListener('click', () => {
+//     galleryImage.src = `images/${gallerySrc[(slideIdx < galleryLength - 1)
+//         ? ++slideIdx : slideIdx = 0]}`;
+//     const desc = galleryImgDesc[slideIdx];
+//     galleryDesc.innerHTML = desc;
+// });
+
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+const galleryImage = document.querySelector('.gallery-img');
+let gallery = Object.keys(galleryDict);
+quickSort(gallery, 0, gallery.length - 1);
 const galleryDesc = document.querySelector('#img-desc');
-const galleryLength = Object.keys(galleryDict).length;
-const galleryImgDesc = Object.keys(galleryDict);
-const gallerySrc = Object.values(galleryDict);
 let slideIdx = 0;
 
 prevBtn.addEventListener('click', () => {
-    galleryImage.src = `images/${gallerySrc[(slideIdx > 0) ? --slideIdx
-        : slideIdx = galleryLength - 1]}`;
-    const desc = galleryImgDesc[slideIdx];
+    galleryImage.src = `images/${galleryDict[gallery[(slideIdx > 0) ? --slideIdx
+        : slideIdx = gallery.length - 1]]}`;
+    let desc = gallery[slideIdx];
     galleryDesc.innerHTML = desc;
 });
 
 nextBtn.addEventListener('click', () => {
-    galleryImage.src = `images/${gallerySrc[(slideIdx < galleryLength - 1)
-        ? ++slideIdx : slideIdx = 0]}`;
-    const desc = galleryImgDesc[slideIdx];
+    galleryImage.src = `images/${galleryDict[gallery[(slideIdx < gallery.length - 1)
+        ? ++slideIdx : slideIdx = 0]]}`;
+    let desc = gallery[slideIdx];
     galleryDesc.innerHTML = desc;
 });
 
